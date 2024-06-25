9c809f53fc8f9a38f03111a9aef61eb46af914e2
MarcioSeligra <marciofariaseligra@gmail.com>
2024-06-25 11:14:13 -0300

    Create J.c

---
+++ J.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+   int a,b,c;
+   
+   printf("insira o valor de a: ");
+   scanf("%d",&a);
+   
+   printf("insira o valor de b: ");
+   scanf("%d",&b);
+   
+   c=(a-b)*(a-b);
+   
+   printf("%d",c);
+   
+}
