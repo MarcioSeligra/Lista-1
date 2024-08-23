d82ea3de84d511b2eca0fd440869fead6bda1950
MarcioSeligra <marciofariaseligra@gmail.com>
2024-08-23 10:24:18 -0300

    O.c

---
+++ O.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int A,B,C,D,P,S;
+
+    printf("insira o valor de A: ");
+    scanf("%d", &A);
+
+    printf("insira o valor de B: ");
+    scanf("%d", &B);
+
+    printf("insira o valor de C: ");
+    scanf("%d", &C);
+
+    printf("insira o valor de D: ");
+    scanf("%d", &D);
+
+    P=A*C;
+    S=B+D;
+
+
+    printf("valor de P: %d\n",P  );
+    printf("valor de S: %d",S  );
+
+}
